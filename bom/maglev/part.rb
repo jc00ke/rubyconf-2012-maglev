8df4f815207aba9946b1d6c9f4f58f4134a304d9
jc00ke <jesse@jc00ke.com>
2012-10-27 19:44:44 -0700

    Adding initial bom w/just objects

---
+++ bom/maglev/part.rb
@@ -0,0 +1,34 @@
+require 'securerandom'
+require 'set'
+
+class Part
+  attr_accessor :name, :description, :cost, :count
+  attr_reader :part_number, :components, :where_used
+
+  def initialize(opts)
+    @name = opts.fetch(:name)
+    @description = opts.fetch(:description) {
+      "Default part description for #@name"
+    }
+    @cost = opts.fetch(:cost)
+    @count = opts.fetch(:count) { 0 }
+    generate_part_number
+    @components = Set.new
+    @where_used = Set.new
+  end
+
+  def add_component(part)
+    @components << part
+    part.used_in self
+  end
+
+  protected
+  def used_in(part)
+    @where_used << part
+  end
+
+  private
+  def generate_part_number
+    @part_number ||= SecureRandom.hex(12)
+  end
+end
---
+++ bom/maglev/test/test_part.rb
@@ -0,0 +1,33 @@
+require 'test/unit'
+require File.expand_path('../../part.rb', __FILE__)
+
+class TestPart < Test::Unit::TestCase
+  def setup
+    @cost = 10
+    @count = 1
+    @pencil = Part.new({
+      :name => "Pencil",
+      :cost => @cost,
+      :count => @count,
+    })
+  end
+
+  def test_initialization
+    assert_not_nil @pencil.part_number
+    assert_equal @cost, @pencil.cost
+    assert_equal @count, @pencil.count
+    assert_equal Set.new, @pencil.components
+    assert_equal Set.new, @pencil.where_used
+  end
+
+  def test_adding_component
+    eraser = Part.new({
+      :name => "Eraser",
+      :cost => 1,
+      :count => 10
+    })
+    @pencil.add_component eraser
+    assert @pencil.components.include?(eraser)
+    assert eraser.where_used.include?(@pencil)
+  end
+end
