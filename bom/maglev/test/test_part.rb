a752361ebde6bb2ddbdbcbfb55a3c520a18218a8
jc00ke <jesse@jc00ke.com>
2012-10-27 21:04:55 -0700

    More test objects

--- bom/maglev/test/test_part.rb
+++ bom/maglev/test/test_part.rb
@@ -5,16 +5,12 @@ class TestPart < Test::Unit::TestCase
   def setup
     @cost = 10
     @count = 1
-    @pencil = Part.new({
-      :name => "Pencil",
-      :cost => @cost,
-      :count => @count,
-    })
-    @eraser = Part.new({
-      :name => "Eraser",
-      :cost => 1,
-      :count => 10
-    })
+    @pencil = Part.new(:name => "Pencil", :cost => @cost, :count => @count)
+    @body = Part.new(:name => "Body", :cost => 0, :count => 1)
+    @wood = Part.new(:name => "Pencil wood", :cost => 1, :count => 10)
+    @graphite = Part.new(:name => "Graphite", :cost => 2, :count => 100)
+    @eraser = Part.new(:name => "Eraser", :cost => 1, :count => 10)
+    @fitting = Part.new(:name => "Eraser Fitting", :cost => 1, :count => 10)
   end
 
   def test_initialization
@@ -27,6 +23,7 @@ class TestPart < Test::Unit::TestCase
 
   def test_adding_component
     @pencil.add_component @eraser
+
     assert @pencil.components.include?(@eraser)
     assert @eraser.where_used.include?(@pencil)
   end
